1f2a01a77f450e1c4d77f9a204072c179a9dedc4
Rik <rik.hoekstra@xs4all.nl>
2024-04-16 13:59:51 +0200

    Update numderiv tests with tolerance for numerical derivative computation

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,6 @@ using Test
 @testset "Numerical derivative" begin
     f(x) = x^3
     g(x) = x*x+6
-    @test numderiv(f,3.0) ≈ 27.0
-    @test numderiv(g,3.0) ≈ 6.0
+    @test numderiv(f,3.0) ≈ 27.0 atol=1e-6
+    @test numderiv(g,3.0) ≈ 6.0 atol=1e-6
 end
